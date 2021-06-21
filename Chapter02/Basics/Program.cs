63a37f5ea4df5323a55fb41f50d3fc6ce6c8780b
OAndreae <oliverandreae@outlook.com>
2021-06-21 18:50:22 +0100

    Updated files

---
+++ Chapter02/Basics/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+// Outputs the compiler version to the 'Problems' tab
+// #error version
+
+/*
+    This is a multiline comment
+*/
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare some unused variables using types
+            // in additional assemblies
+            System.Data.DataSet dataSet;
+            System.Net.Http.HttpClient client;
+
+
+            // Loop through the assemblies that this app references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // Load the assembly so that we can read its details
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                // Declare a variable to count the number of methods
+                int methodCount = 0;
+
+                // Loop through all the types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    // Add up the counts of methods
+                    methodCount += t.GetMethods().Count();
+                }
+
+                // Output the count of types and their methods
+                Console.WriteLine(
+                    "{0:N0} types with {1:N0} methods in {2} assembly.",
+                    arg0: a.DefinedTypes.Count(),
+                    arg1: methodCount,
+                    arg2: r.Name);
+
+            }
+        }
+    }
+}
