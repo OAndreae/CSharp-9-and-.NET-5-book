b2bb34bb161cd5577ab831c0f1ffba8720af8f1b
OAndreae <oliverandreae@outlook.com>
2021-06-23 21:49:13 +0100

    Added folder on bitwise and shift operators

---
+++ Chapter03/BitewiseAndShiftOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BitewiseAndShiftOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
