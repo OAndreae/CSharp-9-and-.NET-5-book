062ef2ae898ac9ab491fbcd44b7eb2a5e629e885
OAndreae <oliverandreae@outlook.com>
2021-06-23 21:11:13 +0100

    Added operators examples

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = a++;
+            WriteLine($"Postfix operator: a is {a}; b is {b}");
+
+            int c = 3;
+            int d = ++a; // increment c before assigning it
+            WriteLine($"Prefix operator: a is {c}; b is {d}");
+
+            int e = 11;
+            int f = 3;
+            WriteLine($"e is {e} and f is {f}");
+            WriteLine($"e + f = {e + f}");
+            WriteLine($"e - f = {e - f}");
+            WriteLine($"e * f = {e * f}");
+            WriteLine($"e / f = {e / f}");
+            WriteLine($"e % f = {e % f}");
+
+            double g = 11.0;
+            WriteLine($"g is {g:N1}, f is {f}");
+            WriteLine($"g / f = {g / f}");
+            
+        }
+    }
+}
