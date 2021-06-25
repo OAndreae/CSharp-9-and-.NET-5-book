d026b3deb32979c9b8de4dcaa996cf43c18f9969
OAndreae <oliverandreae@outlook.com>
2021-06-25 16:36:53 +0100

    Added solution to exercise 3.3

---
+++ Chapter03/Exercise03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int max = 100;
+            for (int n = 0; n < max; n++)
+            {
+                string message = n switch
+                {
+                    int x when (x % 3 == 0) && (x % 5 == 0)
+                        => "fizzbuzz",
+                    int x when (x % 3 == 0)
+                        => "fizz",
+                    int x when (x % 5 == 0)
+                        => "buzz",
+                    int x
+                        => x.ToString()
+                };
+
+                WriteLine(message);
+            }
+        }
+    }
+}
