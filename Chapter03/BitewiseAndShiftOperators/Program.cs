d8c49b309cf1f7ce8bcaf4ac44522c0734b58533
OAndreae <oliverandreae@outlook.com>
2021-06-25 12:13:31 +0100

    Added examples of bitwise and shift operators

--- Chapter03/BitewiseAndShiftOperators/Program.cs
+++ Chapter03/BitewiseAndShiftOperators/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace BitewiseAndShiftOperators
 {
@@ -6,7 +7,22 @@ namespace BitewiseAndShiftOperators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 10; // 0000 1010
+            int b = 6; //  0000 0110
+            WriteLine($"a & b = {a & b}"); // AND => 0000 0010
+            WriteLine($"a | b = {a | b}"); // OR => 0000 1110
+            WriteLine($"a ^ b = {a ^ b}"); // XOR => 0000 1100
+
+            // left-shift a by three bit columns => 0101 0000
+            // equivalent to multiplying by 8
+            // CPUs can perform bit-shifting more quickly
+            WriteLine($"a << 3 = {a << 3}");
+
+            // multiply by 8
+            WriteLine($"a * 8 = {a * 8}");
+
+            // right-shift b by one bit column
+            WriteLine($"b >> 1 = {b >> 1}");
         }
     }
 }
