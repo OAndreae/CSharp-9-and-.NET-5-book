414a422615457ad2de15c82fd2f5520e59259bdc
OAndreae <oliverandreae@outlook.com>
2021-06-25 16:20:13 +0100

    Added solution to exercise 3.2

---
+++ Chapter03/Exercise02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                checked
+                {
+                    int max = 500;
+                    for(byte i = 0; i<max; i++)
+                    {
+                        WriteLine(i);
+                    }
+                }    
+            }
+            catch(OverflowException)
+            {
+                WriteLine($"Overflow exception occurred.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{ex.GetType()}: {ex.Message}");
+            }
+        }
+    }
+}
