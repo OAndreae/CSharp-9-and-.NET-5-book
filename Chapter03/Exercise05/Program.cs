c15cc0bf2f6441e3a9fc487652612757313bd48e
OAndreae <oliverandreae@outlook.com>
2021-06-25 17:50:31 +0100

    Added solutions to exercise 3.5

---
+++ Chapter03/Exercise05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercise05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 2 + ++x;
+            WriteLine("int x = 3;");
+            WriteLine("int y = 2 + ++x;");
+            WriteLine($"x: {x,-4} y: {y}");
+            WriteLine();
+
+            x = 3 << 2;
+            y = 10 >> 1;
+            WriteLine("x = 3 << 2;");
+            WriteLine("y = 10 >> 1;");
+            WriteLine($"x: {x,-4} y: {y}");
+            WriteLine();
+            
+            x = 10 & 8;
+            y = 10 | 7;
+            WriteLine("x = 10 & 8;");
+            WriteLine("y = 10 | 7;");
+            WriteLine($"x: {x,-4} y: {y}");
+        }
+    }
+}
