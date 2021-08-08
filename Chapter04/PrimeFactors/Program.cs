3985ccc58abad0b7487e1c844a2500544e220cd4
OAndreae <oliverandreae@outlook.com>
2021-08-08 21:32:00 +0100

    Implemented input loop with exit command

--- Chapter04/PrimeFactors/Program.cs
+++ Chapter04/PrimeFactors/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 using PrimeFactorsLib;
 
 namespace PrimeFactors
@@ -7,7 +7,29 @@ namespace PrimeFactors
     {
         static void Main(string[] args)
         {
-            //PrimeFactorsLib.PrimeFactorDecomposer.PrimeFactors()
+            const string exitCmd = "exit";
+            WriteLine("Please enter positive integers to see their prime factor decomposition.");
+            WriteLine($"Enter '{exitCmd}' to stop.");
+
+            for (string input = ""; true;)
+            {
+                Write("> ");
+                input = ReadLine();
+                if(input.ToUpper() != exitCmd.ToUpper())
+                {
+                    return;
+                }
+
+                bool isInt = int.TryParse(input, out int number);
+                if ((!isInt || number < 2))
+                {
+                    WriteLine($"'{input}' is invalid. Please input positive integers in the range [2, {int.MaxValue:N0}].");
+                    continue;
+                }
+
+                string primeFactors = PrimeFactorDecomposer.PrimeFactors(number);
+                WriteLine($"{input} = {primeFactors}");
+            }
         }
     }
 }
