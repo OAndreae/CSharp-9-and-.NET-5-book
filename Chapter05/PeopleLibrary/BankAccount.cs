3caf0e2d8fbc21b68d7584fce5b4706fd81ff4d9
OAndreae <oliverandreae@outlook.com>
2021-08-09 19:33:07 +0100

    Added useful code from book

---
+++ Chapter05/PeopleLibrary/BankAccount.cs
@@ -0,0 +1,9 @@
+namespace PeopleLibrary.Shared
+{
+    public class BankAccount
+    {
+        public string AccountName;
+        public decimal Balance;
+        public static decimal InterestRate;
+    }
+}
--- Chapter05/PeopleLibrary/Person.cs
+++ Chapter05/PeopleLibrary/Person.cs
@@ -1,8 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+
+using static System.Console;
 
 namespace PeopleLibrary.Shared
 {
-    public class Person
+    // Explicit inheritance from object not needed
+    public class Person : object
     {
+        public string Name;
+        public DateTime DateOfBirth;
+
+        public WondersOfTheAncientWorld FavouriteAncientWonder;
+        public WondersOfTheAncientWorld BucketList;
+
+        public List<Person> Children = new List<Person>();
+
+        // Bad practice - use readonly instead as it is initialised
+        // at runtime, not compile time. This avoids errors when 
+        // recompiling assemblies. It can also be assigned a expression
+        // that is evaluated at runtime (e.g. a function return value).
+        public const string Species = "Homo sapien";
+
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        public Person()
+        {
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        public void WriteToconsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+        }
+
+        public string GetOrigin()
+        {
+            return $"{Name} was born on {HomePlanet}";
+        }
+
+        public (string, int) GetFruit()
+        {
+            return ("Apples", 5);
+        }
+
+        public (string Name, int Count) GetNamedFruit()
+        {
+            return (Name: "Pears", Count: 3);
+        }
+
+        public string SayHello()
+        {
+            return $"{Name} says 'Hello!'";
+        }
+
+        public string SayHello(string name)
+        {
+            return $"{Name} says 'Hello {name}!'";
+        }
+
+        public string OptionalParameters(string command = "Run!", double number = 0.0, bool active = true)
+        {
+            return string.Format(
+              format: "command is {0}, number is {1}, active is {2}",
+              arg0: command, arg1: number, arg2: active);
+        }
     }
 }
