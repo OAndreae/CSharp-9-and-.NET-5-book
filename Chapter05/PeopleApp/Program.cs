a86ba5cac7ed3914386983ca987084589654ed1c
OAndreae <oliverandreae@outlook.com>
2021-08-09 17:11:03 +0100

    Added person instances with different date formatting

---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using PeopleLibrary.Shared;
+using System;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+            WriteLine($"{bob.Name} was born on {bob.DateOfBirth:dddd, d MMMM yyyy}");
+
+            var alice = new Person
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+
+            WriteLine($"{alice.Name} was born on {alice.DateOfBirth:dd MM yy}");
+
+
+        }
+    }
+}
